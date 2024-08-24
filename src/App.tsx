188fe1cb44fbab6053efce035391f8946a5ae10e
45gh4 <45gh4@protonmail.com>
2024-08-24 17:00:19 +0900

    20240824

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ import { Pagination } from '@mui/material';
 
 
 const drawerWidth = 150;
-const data = require("./json/data.json");
-const sellData = require("./json/sellData.json");
+// const data = require("./json/data.json");
+// const sellData = require("./json/sellData.json");
 
 // eslint-disable-next-line
 type jsonType = {
@@ -95,10 +95,12 @@ function App() {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 0 }}>
             Number
           </Typography>
-          <Box sx={{ flexGrow: 0 }} />
+          <Button variant='contained' onClick={() => setSellScreen(true)} sx={{ ml: 4, p: 1.5, fontWeight: "bold", borderRadius: 3, backgroundColor: "#4682b4", display: sellScreen ? "none" : "block" }}>👑VIP</Button>
+          <Button variant='contained' onClick={() => handleClickOpen()} sx={{ ml: 4, p: 1.5, fontWeight: "bold", borderRadius: 3, backgroundColor: "#4682b4", display: !sellScreen ? "none" : "block" }}>How to Charge</Button>
+          <Box sx={{ flexGrow: 1 }} />
           <IconButton
             size="large"
             edge="start"
@@ -153,7 +155,7 @@ function App() {
       <a href="https://info.flagcounter.com/Bawc">
         <img src="https://s11.flagcounter.com/count2/Bawc/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_10/viewers_0/labels_0/pageviews_0/flags_0/percent_0/" alt="Flag Counter" style={{display: "none"}}/>
       </a>
-      <Button variant='contained' onClick={() => setSellScreen(true)} sx={{ m: 1, p: 2, mb: -2, fontWeight: "bold", borderRadius: 3, backgroundColor: "#483d8b", display: sellScreen ? "none" : "block" }}>👑VIP</Button>
+      {/* <Button variant='contained' onClick={() => setSellScreen(true)} sx={{ m: 1, p: 2, mb: -2, fontWeight: "bold", borderRadius: 3, backgroundColor: "#483d8b", display: sellScreen ? "none" : "block" }}>👑VIP</Button> */}
       { !sellScreen && jsonData && (
         <Box sx={{ m: 0 }}>
           <Grid container spacing={1} sx={{ mb: 4 }}>
@@ -186,7 +188,7 @@ function App() {
       { sellScreen && jsonSellData && (
         <Box sx={{ m: 0 }}>
           <Grid container spacing={1} sx={{ mb: 4 }}>
-            <Button variant='contained' onClick={() => handleClickOpen()} sx={{ m: 2, p: 2, mb: -2, fontWeight: "bold", borderRadius: 8, backgroundColor: "#228b22" }}>How to Charge</Button>
+            {/* <Button variant='contained' onClick={() => handleClickOpen()} sx={{ m: 2, p: 2, mb: -2, fontWeight: "bold", borderRadius: 8, backgroundColor: "#228b22" }}>How to Charge</Button> */}
           {
             sellPaginatedData.map((data: selljsonType) => {
               return (
